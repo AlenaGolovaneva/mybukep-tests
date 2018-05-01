af67cbd199fad45c07f4e9d76862e65e3faa77ee
AlenaGolovaneva <golovaneva.93@mail.ru>
2018-05-01 12:01:24 +0300

    Add new method GetElement

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -12,53 +13,59 @@ namespace MyBukepTests
         {
             Program program = new Program();
             string testName = Console.ReadLine();
-            switch(testName){
-                case "Login":
-                program.LoginTest();
-                break;
-                case "SendEmail":
-                program.SendEmailTest();
-                break;
-
-            }
-
+            switch (testName)
             {
-            IWebElement GetElement (By locator)
-            List<IWebElement> elements = Browser.FindElements(Locator).Tolist();
-            if private static By locator;
-            (elements.Count>0) return elements[0];
-            else return null;
+                case "Login":
+                    program.LoginTest();
+                    break;
+                case "SendEmail":
+                    program.SendEmailTest();
+                    break;
+
             }
-            
-        }
 
-        public void SendEmailTest(){
+
 
         }
-        
+
+        public IWebElement GetElement(By locator)
+        {
+            List<IWebElement> elements = Browser.FindElements(locator).ToList();
+            if (elements.Count > 0)
+                return elements[0];
+            else
+                return null;
+        }
+
+        public void SendEmailTest()
+        {
+
+        }
 
 
-        public void LoginTest() {
 
-            
+        public void LoginTest()
+        {
+
+
             Browser = new ChromeDriver("/Users/alenagolovaneva/Documents/Repositories/MyBukepTests/bin/Debug/netcoreapp2.0/");
             Browser.Manage().Window.Maximize();
             Browser.Navigate().GoToUrl("https://my.bukep.ru:447");
             IWebElement loginInput = Browser.FindElement(By.Id("login"));
             IWebElement passwordInput = Browser.FindElement(By.Id("password"));
-            IWebElement tofind = GetElement(By.ClassName(""));
+            IWebElement tofind = GetElement(By.LinkText("Расписание"));
             IWebElement battonInput = Browser.FindElement(By.CssSelector(".btn.btn-block.btn-primary"));
             loginInput.SendKeys("vtest");
             passwordInput.SendKeys("vtest");
             battonInput.Click();
-            
+
             if (tofind != null)
             {
-                Console.WriteLine("GOOD");
+                Console.WriteLine("Test Passed :)");
             }
             else
             {
-                Console.WriteLine("BUG");
+                Console.WriteLine("Test Failed :(");
 
             }
         }
